a15bddd858eb6594c820a58b3eb74e6cad8a78d7
Wesley Teles <wesley.teles.dev@gmail.com>
2022-04-06 22:15:01 -0300

    JavaScript Criado + Ajustes no CSS e README

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
+[22-03-22]
+
 Projeto Criada para Estudar alguns principios basicos do HTML CSS e JavaScript
+A ideia do projeto é criar um jogo de pedra papel e tesoura utilizando condições if e else
+e operadores matematicos do javaScript.
 
-A ideia do projeto é criar um jogo de pedra papel e tesoura utilizando condições id e else
-e operadores matematicos do javaScript
+![Jogo Pedra Papel e Tesoura](assets/img/Screenshot_1.png)
 
-Primeira Parte Criada [22-03-22]
-    - Estrutura Inicial Html
-    - Estutura Inicial Css
-    - Criação do Readme
+Primeira Parte Criada 
+- Estrutura Inicial Html
+- Estutura Inicial Css
+- Criação do Readme
 
-Segunda Parte
+
+[06-04-2022]
+
+Segunda Parte Criada
+
+- JavaScript Estruturado porem precisando refatorar
+- Ajustes no CSS
+
+Atualizações Futuras
+
+- Colocar um input para o usuario colocar seu nome e ficar no placar
+- Fazer com que mostre oque o computador selecionou
+
+
+Stack Utulizada 
+
+- HTML
+- CSS
+- JavaScript
+
+Como Utilizar
+
+- Abra o index.html em seu melhor navegador e divirta-se 
---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+var jogadorEscolha = 0;
+var computadorEscolha = 0;
+var ganhador = -1
+var pontuacaoJogador = 0;
+var computadorPontuacao = 0;
+// Variaveis Declarada para escolha do jogador e do computador sendo 0 = a empate
+
+function jogar (escolha) {
+    jogadorEscolha = escolha;
+    computadorEscolha = Math.floor((Math.random() * (3 - 1 + 1 )) + 1);
+    //utilizei o radom para ele sortear numeros aleatorio porem fiz uma conta para nao passar de 3 e usei o floor para
+    //arredondar para baixo.
+    
+    // 1 = Pedra
+    // 2 = Papel
+    // 3 = Tesoura
+    if((jogadorEscolha == 1) && (computadorEscolha == 1)) {
+        ganhador = 0;
+    }
+    else if((jogadorEscolha == 1) && (computadorEscolha == 2)) {
+        ganhador = 2;
+    }
+    else if((jogadorEscolha == 1 ) && (computadorEscolha == 3)) {
+        ganhador = 1;
+    }
+
+    // >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<
+
+    else if((jogadorEscolha == 2) && (computadorEscolha == 1)) {
+        ganhador = 1;
+    }
+    else if((jogadorEscolha == 2) && (computadorEscolha == 2)) {
+        ganhador = 0;
+    }
+    else if((jogadorEscolha == 2 ) && (computadorEscolha == 3)) {
+        ganhador = 2;
+    }
+
+    // >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<
+
+    else if((jogadorEscolha == 3) && (computadorEscolha == 1)) {
+        ganhador = 2;
+    }
+    else if((jogadorEscolha == 3) && (computadorEscolha == 2)) {
+        ganhador = 1;
+    }
+    else if((jogadorEscolha == 3 ) && (computadorEscolha == 3)) {
+        ganhador = 0;
+    }
+
+    // >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<
+
+    if(ganhador == 0) {
+        document.querySelector('#mensagem').innerHTML = 'Empate';
+    }
+    else if(ganhador == 1) {
+        document.querySelector('#mensagem').innerHTML = 'Jogador Ganhou';
+        pontuacaoJogador++;
+    }
+    else if(ganhador == 2) {
+        document.querySelector('#mensagem').innerHTML = 'Computador Ganhou';
+        computadorPontuacao++;
+    }
+
+    document.querySelector('#score-people').innerHTML = pontuacaoJogador;
+    document.querySelector('#score-machine').innerHTML = computadorPontuacao;
+
+    if(pontuacaoJogador == 1){
+        Swal.fire('Any fool can use a computer')
+        document.location.reload(true);
+    }
+    if(computadorPontuacao == 1){
+        Swal.fire('Computador Ganhou!')
+        document.location.reload(true);
+    }
+}
+
+
